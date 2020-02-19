491574651edffbab2f840838fe05adf3526f562d
RaphaelFranquim <raphael.franquim@gmail.com>
2020-02-19 17:44:47 -0300

    Adicionando index.php com link do exercício da barbearia

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+	<meta charset = "utf-8" />
+	<title> Exercícios </title>
+</head>
+<body>
+	<h1> Página com links dos meus exercícios </h1>
+	<p>
+		Para acessar o site da Barbearia<a href = "20200219/Barbearia/index.php"> clique aqui. </a>
+	</p>
+</body>
+</html>

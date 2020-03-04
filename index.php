b0ffd89b86a6dc96111f42f0801ccd57cf34fda0
RaphaelFranquim <raphael.franquim@gmail.com>
2020-03-04 17:37:47 -0300

    Atualização com o link certo

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 <body>
 	<h1> Página com links dos meus exercícios </h1>
 	<p>
-		Para acessar o site da Barbearia<a href = "Barbearia/index.php"> clique aqui. </a>
+		Para acessar o site da Barbearia<a href = "Barbearia"> clique aqui. </a>
 	</p>
 	<p>
-		Para acessar o site do Cadastro<a href = "20200304/index.php"> clique aqui. </a>
+		Para acessar o site do Cadastro<a href = "20200304"> clique aqui. </a>
 	</p>
 </body>
 </html>

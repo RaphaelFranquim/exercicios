36732f35d9b724b1f25c0c5b956e8f6f852828d6
RaphaelFranquim <raphael.franquim@gmail.com>
2020-03-04 17:35:04 -0300

    Atualização com o link do index.php certo

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 <body>
 	<h1> Página com links dos meus exercícios </h1>
 	<p>
-		Para acessar o site da Barbearia<a href = "20200219/Barbearia/index.php"> clique aqui. </a>
+		Para acessar o site da Barbearia<a href = "Barbearia/index.php"> clique aqui. </a>
 	</p>
 	<p>
-		Para acessar o site do Cadastro<a href = "20200304/exercicio/index.php"> clique aqui. </a>
+		Para acessar o site do Cadastro<a href = "20200304/index.php"> clique aqui. </a>
 	</p>
 </body>
 </html>
